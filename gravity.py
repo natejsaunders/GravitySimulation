1c05d268b19bb9f6312ea8ae3828c819302c5abf
Nate Saunders <natejsaunders04@gmail.com>
2024-05-13 22:29:55 +0100

    gravity implemented

--- gravity.py
+++ gravity.py
@@ -1,3 +1,5 @@
+import math
+
 import pygame
 
 from gravity_object import GravityObject
@@ -5,9 +7,14 @@ from gravity_object import GravityObject
 SCREEN_SIZE = pygame.Vector2(1280, 720)
 BACKGROUND_COLOR = pygame.Color(0, 0, 0) # black
 MAX_FRAMERATE = 60 # (fps)
+BASE_TIME_MODIFIER = 10
+
+time_modifier = BASE_TIME_MODIFIER
+
+paused = False
 
 # Need to eyeball this
-G = 1
+G = 6.67430 * (10^-11)
 
 screen = pygame.display.set_mode(SCREEN_SIZE)
 clock = pygame.time.Clock()
@@ -18,25 +25,59 @@ delta = 0
 gravity_objects = []
 
 # Test objects
-gravity_objects.append(GravityObject(velocity=pygame.Vector2(1,1), radius = 5))
-gravity_objects.append(GravityObject(position=pygame.Vector2(SCREEN_SIZE.x/2, SCREEN_SIZE.y/2), radius = 30, density=10))
+gravity_objects.append(GravityObject(position=pygame.Vector2(200, SCREEN_SIZE.y/2), velocity=pygame.Vector2(0, 4.6875), radius = 3))
+gravity_objects.append(GravityObject(position=pygame.Vector2(400, SCREEN_SIZE.y/2), velocity=pygame.Vector2(0, 0), radius = 10, fixed=True))
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                paused = not paused
 
-    screen.fill(BACKGROUND_COLOR)
 
-    # Calculating new object values and drawing
-    for gravity_object in gravity_objects:
-        # Updating position and velocity values for now
-        gravity_object.update()
+    if not paused:
+        screen.fill(BACKGROUND_COLOR)
+        # Calculating new object values and drawing
+        for gravity_object in gravity_objects:
 
-        #pygame.draw.circle(screen, gravity_object.color, gravity_object.position, gravity_object.radius)
-        gravity_object.draw(screen)
+            g_force = pygame.Vector2(0, 0)
+            c_force = pygame.Vector2(0, 0)
 
-    pygame.display.flip()
-    delta = clock.tick(MAX_FRAMERATE) / 1000
+            # Iterating throug other objects and applying gravity
+            for other_gravity_object in gravity_objects:
+                # Don't calculate for itself
+                if gravity_object == other_gravity_object:
+                    continue
+
+                rel_pos = gravity_object.position - other_gravity_object.position
+                distance = rel_pos.magnitude()
+                direction = rel_pos / distance
+
+                g_force_scalar = ((G * gravity_object.mass * other_gravity_object.mass) / math.pow(distance, 2.0))
+
+                c_force_scalar = (gravity_object.mass * gravity_object.velocity.magnitude()) / distance
+
+                # Adding centrifugal force from possible orbit
+                c_force.x += c_force_scalar * direction.x
+                c_force.y += c_force_scalar * direction.y
+
+                g_force.x += g_force_scalar * -direction.x
+                g_force.y += g_force_scalar * -direction.y
+
+                if not gravity_object.fixed: print(g_force_scalar)
+
+                pygame.draw.aaline(screen, pygame.Color(255,255,255), gravity_object.position, gravity_object.position + direction * (-g_force_scalar*100))
+                
+            # Updating position and velocity values for now
+            gravity_object.update(g_force, delta)#)c_force
+
+            #pygame.draw.circle(screen, gravity_object.color, gravity_object.position, gravity_object.radius)
+            gravity_object.draw(screen)
+            
+        pygame.display.flip()
+
+    delta = clock.tick(MAX_FRAMERATE) / 1000.0 * time_modifier
 
 pygame.quit()
--- gravity_object.py
+++ gravity_object.py
@@ -1,27 +1,44 @@
 import random
+import math
 
 import pygame
 
 
 class GravityObject:
 
-    def __init__(this, radius=100, density=1, position=pygame.Vector2(0,0), velocity=pygame.Vector2(0,0), acceleration=pygame.Vector2(0,0)):
+    id_counter = 1
+
+    def __init__(self, radius=100, density=1, position=pygame.Vector2(0,0), velocity=pygame.Vector2(0,0), acceleration=pygame.Vector2(0,0), fixed=False):
+        self.id = GravityObject.id_counter
+        GravityObject.id_counter += 1
+
+        self.radius = radius # Pixels
+        self.density = density # kg/pixel
+
+        self.mass = (math.pi * math.pow(self.radius, 2)) * self.density
         
-        this.radius = radius # Pixels
-        this.density = density # kg/pixel
-        
-        this.position = position
-        this.velocity = velocity # pixels/frame
-        this.acceleration = acceleration # pixels/frame^2
+        self.position = position.copy()
+        self.velocity = velocity.copy() # pixels/frame
+        self.acceleration = acceleration.copy() # pixels/frame^2
 
-        this.color = pygame.color.Color(random.randint(100,255),random.randint(100,255),random.randint(100,255))
+        self.fixed = fixed
 
-    def update(this):
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-        this.velocity.x += this.acceleration.x
-        this.velocity.y += this.acceleration.y
+        self.color = pygame.color.Color(random.randint(100,255),random.randint(100,255),random.randint(100,255))
 
-    def draw(this, screen):
-        print(this.position)
-        pygame.draw.circle(screen, this.color, this.position, this.radius)
+    def update(self, force, delta):
+        if self.fixed: return
+
+        self.acceleration.x += force.x / self.mass * delta
+        self.acceleration.y += force.y / self.mass * delta
+
+        self.velocity += self.acceleration * delta
+        self.position += self.velocity * delta
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.color, self.position, self.radius)
+
+    def __eq__(self, value: object) -> bool:
+        if type(value) != GravityObject:
+            return False
+
+        return self.id == value.id
